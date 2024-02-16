d7cb82592811a00aee38eed3597845171cca46c6
wldus1208 <ji915405@gmail.com>
2024-02-16 14:01:18 +0900

    diary 목록

---
+++ src/main/java/kr/happyjob/study/vo/diary/DiaryVO.java
@@ -0,0 +1,54 @@
+package kr.happyjob.study.vo.diary;
+
+public class DiaryVO {
+	private String loginId;
+	private String d_title;
+	private String d_weather;
+	private String d_mood;
+	private String d_contents;
+	private String d_diarydt;
+	private String d_regdate;
+	
+	public String getLoginId() {
+		return loginId;
+	}
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+	public String getD_title() {
+		return d_title;
+	}
+	public void setD_title(String d_title) {
+		this.d_title = d_title;
+	}
+	public String getD_weather() {
+		return d_weather;
+	}
+	public void setD_weather(String d_weather) {
+		this.d_weather = d_weather;
+	}
+	public String getD_mood() {
+		return d_mood;
+	}
+	public void setD_mood(String d_mood) {
+		this.d_mood = d_mood;
+	}
+	public String getD_contents() {
+		return d_contents;
+	}
+	public void setD_contents(String d_contents) {
+		this.d_contents = d_contents;
+	}
+	public String getD_diarydt() {
+		return d_diarydt;
+	}
+	public void setD_diarydt(String d_diarydt) {
+		this.d_diarydt = d_diarydt;
+	}
+	public String getD_regdate() {
+		return d_regdate;
+	}
+	public void setD_regdate(String d_regdate) {
+		this.d_regdate = d_regdate;
+	}
+}
